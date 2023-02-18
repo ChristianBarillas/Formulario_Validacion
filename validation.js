7ff139329d7db5b0d30e297b2a4dfcce666f31e7
christian barillas <70426461+ChristianBarillas@users.noreply.github.com>
2023-02-18 13:28:13 -0600

    mejorando legibilidad del codigo

--- validation.js
+++ validation.js
@@ -1,62 +1,41 @@
-window.onload = function (){
+window.onload = function () {
+  const nombre = document.getElementById("name");
+  const correo = document.getElementById("email");
+  const coemntario = document.getElementById("comment");
+  const boton = document.getElementById("btn");
+  const resultado = document.querySelector(".resultado");
 
-const nombre = document.getElementById("name");
-const correo = document.getElementById("email");
-const coemntario = document.getElementById("comment");
-const boton = document.getElementById("btn");
-const resultado = document.querySelector('.resultado');
+  boton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let error = validarcampo();
+    if (error[0]) {
+      resultado.innerHTML = error[1];
+      resultado.classList.add("red");
+    } else {
+      resultado.innerHTML = "solicitud enviada con exito";
+      resultado.classList.add("green");
+      resultado.classList.remove("red");
+    }
+  });
 
+  const validarcampo = () => {
+    let error = [];
 
-boton.addEventListener('click', (e)=>{
-e.preventDefault();
-let error = validarcampo();
-if (error[0]){
+    if (nombre.value.length < 5 || nombre.value.length > 40) {
+      error[0] = true;
+      error[1] = "el nombre no ouede contener menos de 5";
+      return error;
+    } else if (
+      correo.value.length > 40 ||
+      correo.value.indexOf("@") == -1 ||
+      correo.value.indexOf(".com") == -1
+    ) {
+      error[0] = true;
+      error[1] = "el mail es invalido";
+      return error;
+    }
 
-    resultado.innerHTML = error [1];
-    resultado.classList.add('red')
-
-
-
-} 
-
-else {
-
-    resultado.innerHTML = 'solicitud enviada con exito';
-    resultado.classList.add('green');
-    resultado.classList.remove('red');
-}
-
-
-})
-
-
-
-
-
-
-const validarcampo = () =>{
-let error = [];
-
-if(nombre.value.length < 5 ||  nombre.value.length > 40){
-error[0] = true;
-error[1] = 'el nombre no ouede contener menos de 5'
-return error;
-
-}
-
-
-else if (correo.value.length > 40 || correo.value.indexOf('@') == -1 || correo.value.indexOf('.com') == -1){
-
-    error[0] = true;
-    error[1] = 'el mail es invalido'
+    error[0] = false;
     return error;
-
-}
-
-error[0] = false;
-return error;
-
-}
-
-
-}
+  };
+};
